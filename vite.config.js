b02c5f1915b23456061697722ec4d11d48dcd85c
Michael Bergsten <michaelbergsten@Michaels-MacBook-Air.local>
2023-04-14 16:47:39 +0200

    h

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/doggydaycare/',
+  base: "https://majkcb.github.io/doggydaycare",
   plugins: [react()],
 })
